97f95f7ca98be28995f96d99a7299a07ba9f3929
kArtheen97 <53791739+kArtheen97@users.noreply.github.com>
2019-09-22 11:29:38 +0530

    Created receptionist class

--- app/src/main/java/com/lifelinemadproject/lifelinehospital/Doctor/Doctor.java
+++ app/src/main/java/com/lifelinemadproject/lifelinehospital/Doctor/Doctor.java
@@ -1,6 +1,95 @@
 package com.lifelinemadproject.lifelinehospital.Doctor;
 
 //Creating class to record doctor details
-public class Doctor {
+public class Doctor  {
 
+    private String stfID;
+    private String NIC;
+    private String fName;
+    private String lName;
+    private String contNum;
+    private String userName;
+    private String password;
+    private String nationality;
+    private String sex;
+    private Integer age;
+
+    public Doctor() {
+    }
+
+    public String getStfID() { return stfID; }
+
+    public void setStfID(String stfID) { this.stfID = stfID; }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getContNum() {
+        return contNum;
+    }
+
+    public void setContNum(String contNum) {
+        this.contNum = contNum;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
