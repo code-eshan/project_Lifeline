4963ac7ae025dd1f5e018215038eb039dd6d4972
kArtheen97 <53791739+kArtheen97@users.noreply.github.com>
2019-09-22 11:13:18 +0530

    Created receptionist class

--- app/src/main/java/com/lifelinemadproject/lifelinehospital/ReceptionistRegistrationUI.java
+++ app/src/main/java/com/lifelinemadproject/lifelinehospital/ReceptionistRegistrationUI.java
@@ -8,15 +8,37 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.lifelinemadproject.lifelinehospital.Receptionist.Receptionist;
+
 public class ReceptionistRegistrationUI extends AppCompatActivity {
+
+    EditText sID,first,last,username,password;
+
+    Button btnRecReg;
+
+    DatabaseReference dbRef;
+    Receptionist receptionist;
+
+    public static String key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receptionist_registration_ui);
+
+        //Initiating All Fields
+        sID = (EditText)findViewById(R.id.etRecStaffID);
+        first = (EditText)findViewById(R.id.etRecFirstName);
+        last = (EditText)findViewById(R.id.etRecLastName);
+        username = (EditText)findViewById(R.id.etRecUsername);
+
+
+
 
         Button btnRecReg = (Button)findViewById(R.id.btnRegisterReceptionist);
 
