f23f69f67a04a4947700e62408a9528ad3c61dc5
kArtheen97 <53791739+kArtheen97@users.noreply.github.com>
2019-09-25 23:57:55 +0530

    Completed Receptionist Side1

--- app/src/main/java/com/lifelinemadproject/lifelinehospital/DoctorList.java
+++ app/src/main/java/com/lifelinemadproject/lifelinehospital/DoctorList.java
@@ -16,8 +16,17 @@ import com.lifelinemadproject.lifelinehospital.Appointment.Appointment;
 import com.lifelinemadproject.lifelinehospital.Doctor.Doctor;
 
 import java.util.ArrayList;
+import java.util.EventListener;
 
 public class DoctorList extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference ref;
+    ArrayList<String>list;
+    ArrayAdapter<String> adapter;
+    Doctor doctor;
+
+    ListView ListView1;
 
 
 
@@ -26,6 +35,34 @@ public class DoctorList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_list);
 
+
+        doctor =  new Doctor();
+        ListView1 = (ListView)findViewById(R.id.ListView1);
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("Doctor");
+
+
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this,R.layout.activity_nurappoints,R.id.doctor_user_info,list);
+
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot da:dataSnapshot.getChildren()) {
+                    doctor = da.getValue(Doctor.class);
+                    list.add("Name: " + doctor.getfName() + "\n" + "Contact No: " + doctor.getContNum() );
+                }
+                ListView1.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
